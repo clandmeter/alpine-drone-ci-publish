7e29ae200098e355ef9db13c1604967bfc5a67c2
Carlo Landmeter <clandmeter@alpinelinux.org>
2019-02-18 21:18:11 +0000

    correct home directory

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,7 +8,7 @@ HOST=ua.alpinelinux.org
 apk -U add rsync openssh-client mosquitto-clients
 (umask 077; echo "$SSH_KEY" > "$HOME"/.ssh/id_ed25519)
 ssh buildozer@$HOST "mkdir -p packages/$REPO/$PR/$ARCH"
-rsync -av --delete-after $HOME/drone/packages/*/$ARCH/*.apk \
+rsync -av --delete-after /home/buildozer/drone/packages/*/$ARCH/*.apk \
 	buildozer@$HOST:packages/$REPO/$PR/$ARCH/
 mosquitto_pub -h msg.alpinelinux.org -p 8883 --cafile /etc/ssl/cert.pem \
 	-u user-aports -P "$MQTT_PASS" \
