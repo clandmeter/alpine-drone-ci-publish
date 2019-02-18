b24d4d3d81ecee2b64238a9523f73dafbf7b06b1
Carlo Landmeter <clandmeter@gmail.com>
2019-02-18 20:17:10 +0100

    Initial commit

---
+++ scripts/publish.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ARCH="$(apk --print-arch)"
+PR="$DRONE_PULL_REQUEST"
+REPO="$DRONE_REPO"
+HOST=ua.alpinelinux.org
+
+sudo apk -U add rsync openssh-client mosquitto-clients
+(umask 077; echo "$SSH_KEY" > "$HOME"/.ssh/id_ed25519)
+ssh buildozer@$HOST "mkdir -p packages/$REPO/$PR/$ARCH"
+rsync -av --delete-after $HOME/drone/packages/*/$ARCH/*.apk \
+	buildozer@$HOST:packages/$REPO/$PR/$ARCH/
+mosquitto_pub -h msg.alpinelinux.org -p 8883 --cafile /etc/ssl/cert.pem \
+	-u user-aports -P "$MQTT_PASS" \
+	-t "user-aports" -m "{\"pr\": \"$PR\", \"arch\": \"$ARCH\", \"repo\": \"$REPO\"}"
